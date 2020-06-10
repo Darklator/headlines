35a661daa9437c248b87a1b9c4ed29a38d1bebe4
Dmitry <dimaion666@gmail.com>
2020-06-10 14:35:01 +0500

    Weather

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import urllib2
+import urllib
 
 app = Flask(__name__)
 
@@ -9,8 +12,42 @@ RSS_FEEDS = { 'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
               'cnn': 'http://rss.cnn.com/rss/edition.rss',
               'fox': 'http://feeds.foxnews.com/foxnews/latest',
               'iol': 'http://www.iol.co.za/cmlink/1.640'}
+DEFAULTS = {'publication':'bbc', 'city':'London, UK'}
 
 @app.route("/")
+def home():
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather)
+    
+def get_news(query):
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS["publication"]
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+    
+def get_weather(query):
+    query = urllib.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather =
+         {'description':parsed['weather'][0]['description'],
+         'temperature':parsed['main']['temp'],
+         'city':parsed['name']
+         }
+    return weather
+
 def get_news():
     query = request.args.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
@@ -18,7 +55,20 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    weather = get_weather("London,UK")
+    return render_template("home.html", articles=feed['entries'],weather=weather)
+    
+def get_weather(query):
+     api_url = http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=7a65b0c93b3a79b42eb7474d52cf1846
+     query = urllib.quote(query)
+     url = api_url.format(query)
+     data = urllib2.urlopen(url).read()
+     parsed = json.loads(data)
+     weather = None
+     if parsed.get("weather"):
+         weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]
+     }
+     return weather
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
