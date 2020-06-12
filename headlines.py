64d40e1b7f4a5274683eeb9f893852a2da1350c0
Darklator <61228826+Darklator@users.noreply.github.com>
2020-06-12 13:43:52 +0500

    Add files via upload

--- headlines.py
+++ headlines.py
@@ -3,8 +3,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
-import urllib2
 import urllib
+import urllib.parse
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
@@ -12,7 +13,10 @@ RSS_FEEDS = { 'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
               'cnn': 'http://rss.cnn.com/rss/edition.rss',
               'fox': 'http://feeds.foxnews.com/foxnews/latest',
               'iol': 'http://www.iol.co.za/cmlink/1.640'}
-DEFAULTS = {'publication':'bbc', 'city':'London, UK'}
+DEFAULTS = {'publication':'bbc', 'city':'London, UK', 'currency_from':'GBP','currency_to':'USD'}
+
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=7a65b0c93b3a79b42eb7474d52cf1846"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=9fb6ae75ad3d4a18bdb0ca88f6c6fde1"
 
 @app.route("/")
 def home():
@@ -24,7 +28,14 @@ def home():
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("home.html", articles=articles, weather=weather)
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html", articles=articles, weather=weather, currency_from=currency_from,currency_to=currency_to,rate=rate, currencies=sorted(currencies))
     
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
@@ -35,40 +46,25 @@ def get_news(query):
     return feed['entries']
     
 def get_weather(query):
-    query = urllib.quote(query)
+    query = urllib.parse.quote(query)
     url = WEATHER_URL.format(query)
-    data = urllib2.urlopen(url).read()
+    data = urlopen(url).read()
     parsed = json.loads(data)
     weather = None
     if parsed.get('weather'):
-        weather =
-         {'description':parsed['weather'][0]['description'],
+        weather = {'description':parsed['weather'][0]['description'],
          'temperature':parsed['main']['temp'],
-         'city':parsed['name']
+         'city':parsed['name'],
+         'country':parsed['sys']['country']
          }
     return weather
 
-def get_news():
-    query = request.args.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
-    else:
-        publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather("London,UK")
-    return render_template("home.html", articles=feed['entries'],weather=weather)
-    
-def get_weather(query):
-     api_url = http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=7a65b0c93b3a79b42eb7474d52cf1846
-     query = urllib.quote(query)
-     url = api_url.format(query)
-     data = urllib2.urlopen(url).read()
-     parsed = json.loads(data)
-     weather = None
-     if parsed.get("weather"):
-         weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]
-     }
-     return weather
+def get_rate(frm, to):
+    all_currency = urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate/frm_rate, parsed.keys())
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
